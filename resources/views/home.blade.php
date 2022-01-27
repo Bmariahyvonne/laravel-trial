3a0a9cc0e06ff55f498cc311892bd3b38351a19c
Yvonne <yvonne@marascientific.org>
2022-01-27 11:51:55 +0300

    installing notify package

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,45 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
                     {{ __('You are logged in!') }}
+                </div>
+                <div>
+                    <select class="select-value">
+                        <option value="1">First</option>
+                        <option value="2">Second</option>
+                        <option value="3">Third</option>
+                    </select>
+
+                    <select class="select-value">
+                        <option value="1">First</option>
+                        <option value="2">Second</option>
+                        <option value="3">Third</option>
+                    </select>
+
+                    <select class="select-value">
+                        <option value="1">First</option>
+                        <option value="2">Second</option>
+                        <option value="3">Third</option>
+                    </select>
+
+                    <script>
+                    $(document).on('change', '.select-attendee', function() {
+                        $current = $(this);
+                        $(".select-attendee").not($current).children("option[value='" + $current.val() + "']")
+                            .attr('disabled', "disabled");
+                    });
+                    </script>
                 </div>
             </div>
         </div>
